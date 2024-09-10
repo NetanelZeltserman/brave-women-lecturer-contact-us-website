d202a8f7406ba46cef9f03e6f187f07705c248ef
NatiNati <121944905+NetanelZeltserman@users.noreply.github.com>
2024-09-10 20:23:13 +0300

    feat: modify nodemailer config from gmail to custom smtp server

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -6,21 +6,12 @@ export async function POST(request: NextRequest) {
   const { email, phone, name, message, companyName } = await request.json();
 
   const transport = nodemailer.createTransport({
-    service: 'gmail',
-    /* 
-      setting service as 'gmail' is same as providing these setings:
-
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true
-
-      If you want to use a different email provider other than gmail, you need to provide these manually.
-      Or you can go use these well known services and their settings at
-      https://github.com/nodemailer/nodemailer/blob/master/lib/well-known/services.json
-  */
+    host: process.env.SMTP_HOST,
+    port: parseInt(process.env.SMTP_PORT || '587', 10),
+    secure: process.env.SMTP_SECURE === 'true',
     auth: {
-      user: process.env.MY_EMAIL,
-      pass: process.env.MY_PASSWORD,
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASSWORD,
     },
   });
 
